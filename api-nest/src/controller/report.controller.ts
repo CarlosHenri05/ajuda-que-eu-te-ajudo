4782dd00026d939c6dd4c1df081768754e5b37af
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-07-23 19:56:05 -0300

    refactoring: Adding image to param and adding HttpRequest to validate img

--- api-nest/src/controller/report.controller.ts
+++ api-nest/src/controller/report.controller.ts
@@ -1,22 +1,76 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { ReportDto, ReportResponse } from 'src/dto/report.dto';
 import { ReportService } from 'src/service/report.service';
+import { diskStorage } from 'multer';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+
+// Esse controller ainda não irá funcionar pois o serviço de verificação ainda está sendo CRIADO.
 
 @Controller('report')
 export class ReportController {
-  constructor(private readonly reportService: ReportService) {}
+  constructor(
+    private readonly reportService: ReportService,
+    private readonly httpService: HttpService,
+  ) {}
 
   @Post('create')
-  async createReport(@Body() body: ReportDto): Promise<ReportResponse> {
-    const result = await this.reportService.createReport(body);
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const uniqueName = Date.now() + '-' + file.originalname;
+          cb(null, uniqueName);
+        },
+      }),
+    }),
+  )
+  async createReport(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() body: ReportDto,
+  ): Promise<ReportResponse> {
+    if (!file) {
+      throw new HttpException('File is required', HttpStatus.BAD_REQUEST);
+    }
 
-    /** O Objetivo daqui é ter um serviço em Python
-     * que irá validar a imagem e retornar se ela é válida ou não.
-     * Motivo: A imagem pode conter conteúdo impróprio ou ilegal.
-     */
-    return {
-      approved: true,
-      message: 'Teste',
-    };
+    try {
+      const response = firstValueFrom(
+        this.httpService.post('http://localhost:5000/verify', {
+          image_url: file.path,
+        }),
+      );
+
+      if (!response.data.approved) {
+        return {
+          approved: false,
+          message: response.data.message,
+        };
+      }
+
+      await this.reportService.createReport({
+        ...body,
+        imagem_url: `http://localhost:3000/uploads/${file.filename}`,
+      });
+
+      return {
+        approved: true,
+        message: response.data.message,
+      };
+    } catch (error) {
+      throw new HttpException(
+        'Error processing the report',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
