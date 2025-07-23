d54fdb60da1859e85b4a8d1cb741d1f2663037b4
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-07-23 19:55:09 -0300

    feat: Adding new modules

--- api-nest/src/app.module.ts
+++ api-nest/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ReportService } from './service/report.service';
+import { ReportController } from './controller/report.controller';
+import { MulterModule } from '@nestjs/platform-express';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [
+    MulterModule.register({
+      dest: './uploads',
+    }),
+    HttpModule,
+  ],
+  controllers: [ReportController],
   providers: [ReportService],
 })
 export class AppModule {}
