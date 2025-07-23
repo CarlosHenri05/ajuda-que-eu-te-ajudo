a6a1c8a4192a9424622ebd801a2fdc5979133011
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-07-23 19:55:40 -0300

    feat: Creating new directory to store images

--- api-nest/src/main.ts
+++ api-nest/src/main.ts
@@ -1,8 +1,13 @@
+import { join } from 'path';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
