3a60e63b9312da4e70d02cd7d97ddd1131a9d895
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-07-24 20:26:52 -0300

    feat: Adding ServeStaticModule to add static images to /uploads

--- api-nest/src/app.module.ts
+++ api-nest/src/app.module.ts
@@ -3,6 +3,8 @@ import { ReportService } from './service/report.service';
 import { ReportController } from './controller/report.controller';
 import { MulterModule } from '@nestjs/platform-express';
 import { HttpModule } from '@nestjs/axios';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -10,6 +12,10 @@ import { HttpModule } from '@nestjs/axios';
       dest: './uploads',
     }),
     HttpModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
   ],
   controllers: [ReportController],
   providers: [ReportService],
