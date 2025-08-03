5613b1642c9bd2354abd7a03d65efe2e1de826e9
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-08-02 22:42:18 -0300

    refactor: Adding better error handling and exceptions treatments

--- email-service/src/service/email.service.ts
+++ email-service/src/service/email.service.ts
@@ -1,7 +1,7 @@
 import { MailerService } from '@nestjs-modules/mailer';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { EmailBodyDto } from 'src/dto/email.dto';
 import { Email } from 'src/model/email.model';
 
@@ -12,22 +12,67 @@ export class EmailService {
     @InjectModel(Email.name) private readonly emailModel: Model<Email>,
   ) {}
 
-  async sendEmail(emailDto: EmailBodyDto): Promise<void> {
-    await this.mailService.sendMail({
-      to: emailDto.to,
-      from: emailDto.from,
-      subject: emailDto.subject,
-      text: JSON.stringify(emailDto.text),
-    });
+  async sendEmail(emailBody: EmailBodyDto): Promise<void> {
+    try {
+      await this.mailService.sendMail({
+        to: emailBody.to,
+        from: emailBody.from,
+        subject: emailBody.subject,
+        text: JSON.stringify(emailBody.text),
+      });
+
+      await this.saveEmailOnDb(emailBody);
+    } catch (error) {
+      if (error.code === 'ECONNECTION') {
+        throw new HttpException(
+          'This service is unavailable',
+          HttpStatus.SERVICE_UNAVAILABLE,
+        );
+      }
+
+      if (error.code === 'EAUTH') {
+        throw new HttpException(
+          'Authentication failed',
+          HttpStatus.UNAUTHORIZED,
+        );
+      }
+
+      if (error.code === 'EENVELOPE') {
+        throw new HttpException(
+          'Invalid email address',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+
+      throw new HttpException(
+        `Something went wrong: ${error.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  async saveEmailOnDb(emailDto: EmailBodyDto): Promise<Email> {
-    const email = new this.emailModel({
-      to: emailDto.to,
-      from: emailDto.from,
-      subject: emailDto.subject,
-      text: JSON.stringify(emailDto.text)
-    }).save();
+  async saveEmailOnDb(emailBody: EmailBodyDto): Promise<Email> {
+    try {
+      const email = await new this.emailModel({
+        to: emailBody.to,
+        from: emailBody.from,
+        subject: emailBody.subject,
+        text: JSON.stringify(emailBody.text),
+      }).save();
 
-    return email;
+      return email;
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        throw new HttpException(
+          'Bad request, try checking your input fields again',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+
+      throw new HttpException(
+        `Something went wrong: ${error.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
+}
