ed57656f112e6141a23a73bff0f8a223de32c6b9
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-08-08 18:04:54 -0300

    refactor: adding class-validator annotations and adding new bool field

--- api-nest/src/dto/report.dto.ts
+++ api-nest/src/dto/report.dto.ts
@@ -1,30 +1,48 @@
-import { IsNotEmpty } from 'class-validator';
+import {
+  IsBoolean,
+  IsDate,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
 import { Estado } from 'generated/prisma';
 
 export class ReportDto {
   @IsNotEmpty()
+  @IsString()
   readonly autor: string;
 
   @IsNotEmpty()
+  @IsString()
   readonly tipo_problema: string;
 
   @IsNotEmpty()
+  @IsString()
   readonly descricao: string;
 
   @IsNotEmpty()
+  @IsString()
   readonly imagem_url: string;
 
   @IsNotEmpty()
+  @IsEnum(Estado)
   readonly estado: Estado;
 
   @IsNotEmpty()
+  @IsString()
   readonly endereco: string;
 
   @IsNotEmpty()
+  @IsString()
   readonly cep: string;
 
   @IsNotEmpty()
+  @IsDate()
   criado_em: Date;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  enviar_email: boolean;
 }
 
 export interface ReportResponse {
