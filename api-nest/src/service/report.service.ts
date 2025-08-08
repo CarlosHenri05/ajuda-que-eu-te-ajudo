1eab134dacd4e883ec45dea55a55b2f8931d0370
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-08-08 18:03:45 -0300

    feat: Creating report-service

--- api-nest/src/service/report.service.ts
+++ api-nest/src/service/report.service.ts
@@ -1,101 +1,29 @@
-import { HttpStatus, Injectable, HttpException } from '@nestjs/common';
-import { PrismaClient, Report } from 'generated/prisma';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { Report } from 'generated/prisma';
 import { ReportDto } from 'src/dto/report.dto';
+import { ReportRepository } from 'src/repository/report.repository';
 
 @Injectable()
 export class ReportService {
-  constructor(private readonly prisma: PrismaClient) {}
+  constructor(private readonly reportRepository: ReportRepository) {}
 
   async createReport(reportData: ReportDto): Promise<Report> {
-    const report = await this.prisma.report.create({
-      data: reportData,
-    });
-
-    return report;
-  }
-
-  async getReportById(id: number): Promise<Report | null> {
     try {
-      const report = await this.prisma.report.findUnique({
-        where: {
-          id: id,
-        },
-      });
-      return report;
+      const newReport = await this.reportRepository.createReport(reportData);
+
+      return newReport;
     } catch (error) {
-      if (error.code === 'P2025') {
-        throw new HttpException('Report not found', HttpStatus.NOT_FOUND);
-      }
-
-      throw new HttpException(
-        'Internal server error',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
-  }
-
-  async getAllReports(): Promise<Report[]> {
-    try {
-      const reports = await this.prisma.report.findMany();
-
-      return reports;
-    } catch (error) {
-      if (error.code === 'P2025') {
-        throw new HttpException('Report not found', HttpStatus.NOT_FOUND);
-      }
-      throw new HttpException(
-        'Internal server error',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
-  }
-
-  async updateReport(
-    id: number,
-    reportData: Partial<ReportDto>,
-  ): Promise<void> {
-    try {
-      await this.prisma.report.update({
-        where: {
-          id: id,
-        },
-        data: reportData,
-      });
-    } catch (error) {
-      if (error.code === 'P2025') {
-        throw new HttpException('Report not found', HttpStatus.NOT_FOUND);
-      }
-
       if (error.code === 'P2006') {
-        throw new HttpException(
-          'Invalid data provided',
-          HttpStatus.BAD_REQUEST,
-        );
+        throw new BadRequestException('Bad inputs, try again');
       }
 
-      throw new HttpException(
-        'Internal server error',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
-  }
+      console.log('error: ', error);
 
-  async deleteReport(id: number): Promise<void> {
-    try {
-      await this.prisma.report.delete({
-        where: {
-          id: id,
-        },
-      });
-    } catch (error) {
-      if (error.code === 'P2025') {
-        throw new HttpException('Report not found', HttpStatus.NOT_FOUND);
-      }
-
-      throw new HttpException(
-        'Internal server error',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+      throw new InternalServerErrorException('Internal server error');
     }
   }
 }
